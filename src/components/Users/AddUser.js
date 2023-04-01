e9c10ccba7af9510cc24b3dd5f7da776a32e2f2e
hyejin.shin <hyejin.shin@asurion.com>
2023-04-01 20:39:30 +0900

    portal, ref 사용하기

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,62 +1,54 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
   const [error, setError] = useState();
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: "유효하지 않은 입력값입니다.", 
+        title: "유효하지 않은 입력값입니다.",
         message: "유효한 이름과 나이를 입력해주세요. ",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "유효하지 않은 나이입니다.",
         message: "0보다 큰 숫자를 입력해주세요. ",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-  const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
-    setError(null )
-  }
+    setError(null);
+  };
   return (
     <>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
 
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
-          <label htmlFor="username">이름</label> 
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <label htmlFor="username">이름</label>
+          <input id="username" type="text" ref={nameInputRef} />
           <label htmlFor="age">나이</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">추가</Button>
         </form>
       </Card>
